9402368a5fd31d0aa3bd2ccddf3f83ec8e3de012
Zhakyp01 <zhakyp.zhoomartuuluu@gmail.com>
2023-06-23 22:20:32 +0600

    FDJSANMFHESV

--- dev/us-east-1/main.tf
+++ dev/us-east-1/main.tf
@@ -1,9 +1,9 @@
-# resource "aws_vpc" "default" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     Name = "Default VPC"
-#   }
-# }
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Default VPC"
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
